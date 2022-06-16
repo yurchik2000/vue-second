91a11cd61de22f598708826b8df0d3e62f9e6b22
Andrii Ryzhak <andrii.ryzhak@inventorsoft.co>
2022-06-16 13:38:38 +0300

    Fixes

--- src/components/EventItem.vue
+++ src/components/EventItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">         
-     <form>        
+  <div class="container">
+     <form>
         <div class="input__wrapper">
           <label>Event ID: </label>
           <input type="text" :value="item.id" readonly>
@@ -27,68 +27,68 @@
         </div>
         <div class="input__wrapper">
           <label>Organizer: </label>
-          <input type="text" :value="item.organizer" readonly>
+          <input type="text" :value="item.organizer.name" readonly>
         </div>
         <div class="input__wrapper">
           <label>Category: </label>
           <input type="text" :value="item.category" readonly>
         </div>
-<!--        
+<!--
         <div class="attendees__wrapper">
-          <p class="attendees__title">Attendees:</p>          
+          <p class="attendees__title">Attendees:</p>
           <p v-for="subitem of item.attendees" :key="subitem.id"> <span> ID: </span> {{subitem.id}} <span>,  name: </span> {{subitem.name}}</p>
-        </div>      
+        </div>
 -->
-        <p class="attendees__title">Attendees:</p>          
-        <div class="attendees__wrapper" v-for="subitem of item.attendees" :key="subitem.id">
+        <p class="attendees__title">Attendees:</p>
+        <div class="attendees__wrapper" v-for="attendee of item.attendees" :key="attendee.id">
           <label>ID: </label>
-          <input type="text" :value="subitem.id" readonly>          
+          <input type="text" :value="attendee.id" readonly>
           <label>name: </label>
-          <input type="text" :value="subitem.name" readonly>                    
-        </div>      
-     </form>        
+          <input type="text" :value="attendee.name" readonly>
+        </div>
+     </form>
   </div>
 </template>
 
 <script>
-    export default {                    
+    export default {
         props: {
-          id: String,          
-          item: Object,                    
-        },  
+          id: String,
+          item: Object,
+        },
     }
 </script>
 
 <style scoped>
     .container{
         padding: 10px;
-        margin: o auto;
+        margin: 0 auto;
         background: #eeeeee;
         text-align: left;
     }
     form{
-      background: #a0a0a0;      
+      background: #a0a0a0;
       padding: 10px;
       max-width: 800px;
     }
-    .input__wrapper{      
+    .input__wrapper{
       margin-bottom: 10px;
       display: flex;
       justify-content: space-between;
       font-size: 20px;
       cursor: pointer;
-    }    
-    label{      
-      margin-right: 10px;      
     }
-    input{      
+    label{
+      margin-right: 10px;
+    }
+    input{
       padding: 5px;
       font-size: 20px;
       width: 400px;
-    }    
+    }
     .attendees__wrapper{
       font-size: 20px;
-      margin-left: 20px;      
+      margin-left: 20px;
       display: flex;
     }
     .attendees__wrapper label{
@@ -100,5 +100,5 @@
     p {
       font-size: 20px;
       font-weight: 700;
-    }  
+    }
 </style>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-import DataList from '../../db.json'
+import DataList from '../../db.json';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {            //data
@@ -15,22 +15,24 @@ export default new Vuex.Store({
   //   }
   // },
   actions: {
-    fetchProducts (context){
-      // make tha call            
-      context.commit('setEvents', DataList.events)
-    },    
+    fetchProducts(context) {
+      // make tha call
+      context.commit('setEvents', DataList.events);
+    },
+    createEvent({ commit }, event) {
+      commit('ADD_ITEM', event);
+    }
   },
 
   mutations: {
-    setEvents (state, events) {
-      state.events = events
+    setEvents(state, events) {
+      state.events = events;
     },
-    REMOVE_ITEM (state, item) {
-      state.events = state.events.filter(event => event!=item)
+    REMOVE_ITEM(state, item) {
+      state.events = state.events.filter(event => event.id !== item.id);
     },
-    ADD_ITEM (state, item) {
-      // можна перевірити чи Item  з таким ID вже є в списку      
-      return state.events.push(item)      
+    ADD_ITEM(state, item) {
+      return state.events.push(item);
     }
   }
-})
+});
